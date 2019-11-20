85cffb8cafbb1b374a15e28b8617b18cb5f0338c
christianaasare <christiana.asare@turntabl.io>
2019-11-20 18:00:42 +0000

    Added More Maths Tests

--- src/test/java/io/turntabl/springweb/Models/MathsTest.java
+++ src/test/java/io/turntabl/springweb/Models/MathsTest.java
@@ -8,20 +8,41 @@ import static org.junit.jupiter.api.Assertions.*;
 class MathsTest {
 
     @Test
-    void addition() {
+    void additionTwoPositiveNumbers() {
       int sum= Maths.addition(2,3) ;
       assertEquals(5, sum);
+    }
+
+    @Test
+    void additionTwoNegativeNumbers() {
+        int sum= Maths.addition(-2,-3) ;
+        assertEquals(-5, sum);
+    }
+    @Test
+    void additionZeroAndNegativeNumber() {
+        int sum= Maths.addition(0,-3) ;
+        assertEquals(-3, sum);
     }
 
 
 
 
     @Test
-    void subtraction() {
-        int result= Maths.subtraction(2,3) ;
-        assertEquals(-1, result);
+    void subtractionTwoPositiveNumbers() {
+        int result= Maths.subtraction(7,3) ;
+        assertEquals(4, result);
     }
 
+    @Test
+    void subtractionTwoNegativeNumbers() {
+        int result= Maths.subtraction(-2,-3) ;
+        assertEquals(1, result);
+    }
+    @Test
+    void subtractionZeroAndNegativeNumber() {
+        int result= Maths.addition(0,-3) ;
+        assertEquals(-3, result);
+    }
 
 
 }
